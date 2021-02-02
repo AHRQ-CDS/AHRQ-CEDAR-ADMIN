53d9fbfe5ae966d95b5ace6f837f2ccd272350bc
Pete K <krautscheid@mitre.org>
2021-02-02 11:39:21 -0500

    Capture CDS Connect artifact status and test status and identifiers

--- test/integration/cds_connect_importer_test.rb
+++ test/integration/cds_connect_importer_test.rb
@@ -29,20 +29,26 @@ class CdsConnectImporterTest < ActiveSupport::TestCase
 
     artifact_1186 = artifacts.where(title: 'Facilitating Shared Decision Making For People Who Drink Alcohol: A Patient Decision Aid').first
     assert(artifact_1186.present?)
+    assert_equal('CDS-CONNECT-1186', artifact_1186.cedar_identifier)
+    assert_equal('1186', artifact_1186.remote_identifier)
     assert_match(/This CDS artifact identifies patients screened for alcohol use/, artifact_1186.description)
     assert_equal(['alcohol', 'brief intervention', 'decision aid', 'excessive alcohol use'], artifact_1186.keywords)
     assert_equal(['Preventive Health Services', 'Alcohol Drinking', 'Risk Assessment', 'Substance Abuse Detection'], artifact_1186.mesh_keywords)
     assert_match(/node.1186/, artifact_1186.url)
     assert_equal(Date.parse('Thu, 16 Jul 2020'), artifact_1186.published_on)
     assert_equal('Data Summary', artifact_1186.artifact_type)
+    assert_equal('draft', artifact_1186.artifact_status)
 
     artifact_1221 = artifacts.where(title: 'Managing chronic pain with Prescription Drug Monitoring Program (PDMP) medication dispense data').first
     assert(artifact_1221.present?)
+    assert_equal('CDS-CONNECT-1221', artifact_1221.cedar_identifier)
+    assert_equal('1221', artifact_1221.remote_identifier)
     assert_match(/This artifact implements access to Prescription Drug Monitoring/, artifact_1221.description)
     assert_equal(['Chronic Pain', 'Analgesics, Opioid', 'Prescription Drug Misuse', 'risk assessment', 'Pain Assessment'], artifact_1221.keywords)
     assert_equal(['Analgesics, Opioid', 'Opioid-Related Disorders', 'Pain Management'], artifact_1221.mesh_keywords)
     assert_match(/node.1221/, artifact_1221.url)
     assert_nil(artifact_1221.published_on)
     assert_equal('Data Summary', artifact_1221.artifact_type)
+    assert_equal('draft', artifact_1186.artifact_status)
   end
 end
