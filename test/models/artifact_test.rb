6e68422f680ab8d8eb6a546e58f4c9a89218d8e9
Pete K <krautscheid@mitre.org>
2021-03-16 11:13:25 -0400

    Expanded test coverage to handle description assignment edge cases

--- test/models/artifact_test.rb
+++ test/models/artifact_test.rb
@@ -18,6 +18,24 @@ class ArtifactTest < ActiveSupport::TestCase
     assert_equal 'TEXT', artifact.description
   end
 
+  test 'setting description text field with only html field set' do
+    artifact = Artifact.new(description_html: 'HTML')
+    artifact[:description_markdown] = nil
+    artifact.description = 'TEXT AND MARKDOWN'
+    assert_equal 'HTML', artifact.description_html
+    assert_equal "TEXT AND MARKDOWN\n\n", artifact.description_markdown
+    assert_equal 'TEXT AND MARKDOWN', artifact.description
+  end
+
+  test 'setting description text field with only markdown field set' do
+    artifact = Artifact.new(description_markdown: 'MARKDOWN')
+    artifact[:description_html] = nil
+    artifact.description = 'TEXT AND HTML'
+    assert_equal '<p>TEXT AND HTML</p>', artifact.description_html
+    assert_equal "MARKDOWN\n\n", artifact.description_markdown
+    assert_equal 'TEXT AND HTML', artifact.description
+  end
+
   test 'setting description html field with other fields blank' do
     artifact = Artifact.new(description_html: '<h2>Test</h2> <p>Test</p> <script>Test</script>')
     assert_equal 'Test Test Test', artifact.description
@@ -33,6 +51,24 @@ class ArtifactTest < ActiveSupport::TestCase
     assert_equal 'HTML', artifact.description_html
   end
 
+  test 'setting description html field with only markdown field set' do
+    artifact = Artifact.new(description_markdown: 'MARKDOWN')
+    artifact[:description] = nil
+    artifact.description_html = 'HTML AND TEXT'
+    assert_equal "MARKDOWN\n\n", artifact.description_markdown
+    assert_equal 'HTML AND TEXT', artifact.description
+    assert_equal 'HTML AND TEXT', artifact.description_html
+  end
+
+  test 'setting description html field with only text field set' do
+    artifact = Artifact.new(description: 'TEXT')
+    artifact[:description_markdown] = nil
+    artifact.description_html = 'HTML AND MARKDOWN'
+    assert_equal "HTML AND MARKDOWN\n\n", artifact.description_markdown
+    assert_equal 'TEXT', artifact.description
+    assert_equal 'HTML AND MARKDOWN', artifact.description_html
+  end
+
   test 'setting description markdown field with other fields blank' do
     artifact = Artifact.new(description_markdown: "## Test\n\nTest")
     assert_equal 'Test Test', artifact.description
@@ -46,5 +82,23 @@ class ArtifactTest < ActiveSupport::TestCase
     assert_equal 'HTML', artifact.description_html
     assert_equal 'TEXT', artifact.description
     assert_equal "MARKDOWN\n\n", artifact.description_markdown
+  end
+
+  test 'setting description markdown field with only html field set' do
+    artifact = Artifact.new(description_html: 'HTML')
+    artifact[:description] = nil
+    artifact.description_markdown = 'MARKDOWN AND TEXT'
+    assert_equal 'HTML', artifact.description_html
+    assert_equal 'MARKDOWN AND TEXT', artifact.description
+    assert_equal "MARKDOWN AND TEXT\n\n", artifact.description_markdown
+  end
+
+  test 'setting description markdown field with only text field set' do
+    artifact = Artifact.new(description: 'TEXT')
+    artifact[:description_html] = nil
+    artifact.description_markdown = 'MARKDOWN AND HTML'
+    assert_equal '<p>MARKDOWN AND HTML</p>', artifact.description_html
+    assert_equal 'TEXT', artifact.description
+    assert_equal "MARKDOWN AND HTML\n\n", artifact.description_markdown
   end
 end
