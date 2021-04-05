fb7fd61dc976dd83b4d28660ccd698c2b4788bfa
Yunwei Wang <yunwwang@gmail.com>
2021-04-05 16:52:42 -0500

    PR comments

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -4,6 +4,7 @@
 # indexed by CEDAR.
 class Artifact < ApplicationRecord
   belongs_to :repository
+  after_save :udpate_index 
 
   enum artifact_status: {
     draft: 'draft',
@@ -78,14 +79,18 @@ class Artifact < ApplicationRecord
 
   def self.update_or_create!(cedar_identifier, attributes)
     find_or_initialize_by(cedar_identifier: cedar_identifier).update!(attributes)
-    connection.execute(<<-SQL.squish)
-        UPDATE artifacts SET content_search =#{' '}
-          setweight(to_tsvector('english', coalesce(title, '')), 'A') ||
-          setweight(to_tsvector('english', coalesce(keyword_text, '')), 'B') ||
-          setweight(to_tsvector('english', coalesce(mesh_keyword_text, '')), 'B') ||
-          setweight(to_tsvector('english', coalesce(description, '')), 'D')
-        WHERE cedar_identifier=#{connection.quote(cedar_identifier)}
+  end
+
+  def udpate_index
+    query = <<-SQL.squish
+      UPDATE artifacts SET content_search = (
+        setweight(to_tsvector('english', coalesce(title, '')), 'A') ||
+        setweight(to_tsvector('english', coalesce(keyword_text, '')), 'B') ||
+        setweight(to_tsvector('english', coalesce(mesh_keyword_text, '')), 'B') ||
+        setweight(to_tsvector('english', coalesce(description, '')), 'D'))
+      WHERE id=#{id}
     SQL
+    ActiveRecord::Base.connection.execute(query)
   end
 
   # Return a list of all keywords, regardless of type, with any duplicates pruned
