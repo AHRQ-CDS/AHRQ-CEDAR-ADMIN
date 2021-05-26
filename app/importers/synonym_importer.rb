e2045089711fe3ee11b3d90328fbb05f46768705
Marc Hadley <mhadley@mitre.org>
2021-05-26 13:22:27 -0400

    Add synonym importer for UMLS MRCONSO RRF format

--- app/importers/synonym_importer.rb
+++ app/importers/synonym_importer.rb
@@ -2,6 +2,22 @@
 
 # Functionality for populating the concepts table
 class SynonymImporter
+  SYNONYM_LANGAUGES = ['ENG', 'SPA'].freeze
+
+  def self.import_umls_mrconso(file)
+    concept = ''
+    synonyms = []
+    File.foreach(file) do |line|
+      fields = line.split('|')
+      if fields[0] != concept && concept.present?
+        Concept.create!(name: concept, synonyms_text: synonyms.uniq) if synonyms.size > 1
+        synonyms = []
+      end
+      concept = fields[0]
+      synonyms << fields[14].downcase.strip if SYNONYM_LANGAUGES.include? fields[1]
+    end
+  end
+
   def self.import_mesh(file, canonical_prefix, synonym_prefix)
     canonical_line_start = "#{canonical_prefix} = "
     synonym_line_start = "#{synonym_prefix} = "
