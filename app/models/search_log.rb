e7d4132e0aed401c2cb7f753111096c72ee03915
Yunwei Wang <yunwwang@gmail.com>
2021-07-21 12:25:43 -0500

    Remove duplicated where cluse. Fix typo

--- app/models/search_log.rb
+++ app/models/search_log.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Model for tracking the serach query at CEDAR API
+# Model for tracking the search query at CEDAR API
 class SearchLog < ApplicationRecord
   has_many :search_parameter_logs, dependent: :destroy
 end
--- app/models/search_parameter_log.rb
+++ app/models/search_parameter_log.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Model for tracking the serach query at CEDAR API
+# Model for tracking the search parameters at CEDAR API
 class SearchParameterLog < ApplicationRecord
   belongs_to :search_log
 end
