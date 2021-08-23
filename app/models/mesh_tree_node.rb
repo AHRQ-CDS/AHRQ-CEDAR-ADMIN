175f5487669f8703170ebbbfd0d1d00309d2accf
Diana Eastman <deastman@mitre.org>
2021-08-23 10:05:28 -0400

    Fix rubocop issues.

--- app/models/mesh_tree_node.rb
+++ app/models/mesh_tree_node.rb
@@ -2,8 +2,8 @@
 
 # Represents a MeshTreeNode in the MeshTree hierarchy
 class MeshTreeNode < ApplicationRecord
-  has_many :children, class_name: 'MeshTreeNode', foreign_key: 'parent_id'
-  belongs_to :parent, class_name: 'MeshTreeNode', foreign_key: 'parent_id', optional: true
+  has_many :children, class_name: 'MeshTreeNode', foreign_key: 'parent_id', inverse_of: :parent, dependent: :nullify
+  belongs_to :parent, class_name: 'MeshTreeNode', optional: true, inverse_of: :children
 
   validates :tree_number, presence: true
 
