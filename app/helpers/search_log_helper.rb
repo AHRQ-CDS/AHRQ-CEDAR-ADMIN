ac77ee78e0aa9614e77428f57249ff728d77b612
Adam Barnes <ajbarnes@mitre.org>
2022-01-07 12:09:09 -0500

    Add support for seperating each code search into its own bucket for all supported code systems

--- app/helpers/search_log_helper.rb
+++ app/helpers/search_log_helper.rb
@@ -14,10 +14,32 @@ module SearchLogHelper
     '_count' => 'Count Per Page'
   }.freeze
 
+  CODE_SYSTEMS = {
+    'Metathesaurus' => 'http://www.nlm.nih.gov/research/umls/mth',
+    'MeSH' => 'http://terminology.hl7.org/CodeSystem/MSH',
+    'Medline Plus' => 'http://www.nlm.nih.gov/research/umls/medlineplus',
+    'SNOMED-CT' => 'http://snomed.info/sct',
+    'SNOMED-CT (ESP)' => 'http://snomed.info/sct/449081005',
+    'MeSH (ESP)' => 'http://www.nlm.nih.gov/research/umls/mshspa',
+    'ICD-10-CM' => 'http://hl7.org/fhir/sid/icd-10-cm',
+    'RxNorm' => 'http://www.nlm.nih.gov/research/umls/rxnorm'
+  }.freeze
+
   def human_readable_search_params(search_log)
     search_log.each_with_object({}) do |(key, value), hash|
       value = value.delete('()') if HUMAN_READABLE_PARAMS.key?(key) && !value.is_a?(Array)
       hash[HUMAN_READABLE_PARAMS[key]] = value
     end
+  end
+
+  def seperate_code_systems(code_searches)
+    hash = Hash.new { |k, v| k[v] = [] }
+    # For each search, sort it intro the above bucket based off system's url id
+    code_searches.each do |item|
+      CODE_SYSTEMS.each do |key, value|
+        hash[key].push(item) if item.include? value
+      end
+    end
+    hash
   end
 end
