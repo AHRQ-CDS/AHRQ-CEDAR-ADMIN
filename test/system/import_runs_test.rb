fd98f89798363b6545b054e192d8a29b578a1b4c
Adam Barnes <ajbarnes@mitre.org>
2022-04-26 14:41:40 -0400

    fix: add alt_text to keyword pie charts. Add system tests for import_run and keyword pages

---
+++ test/system/import_runs_test.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class ImportRunsTest < ApplicationSystemTestCase
+  include ApplicationHelper
+
+  setup do
+    sign_in create(:user)
+    @repository = create_repository_with_artifacts(count: 2)
+    @start = Time.current
+    @import_run = create(:import_run, repository: @repository, start_time: @start,
+                                      end_time: Time.current, total_count: 2, new_count: 0, update_count: 0, delete_count: 0)
+  end
+
+  test 'import_run functions as expected' do
+    visit import_run_url(@import_run)
+    assert_selector 'h1', text: "Import Run: #{@repository.name} [#{format_datetime_with_tz(@start)}]"
+    assert_selector 'h4', text: 'Updated [0]'
+    assert_selector 'h4', text: 'Added [0]'
+    assert_selector 'h4', text: 'Deleted [0]'
+    # Assert not shown when empty
+    assert_select 'h4', { count: 0, text: 'Errors [0]' }
+    assert_select 'h4', { count: 0, text: 'Warnings [0]' }
+  end
+
+  test 'import_run is accessible' do
+    visit import_run_url(@import_run)
+    assert_axe_accessible(page)
+  end
+end
