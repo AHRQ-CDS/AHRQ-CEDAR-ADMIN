8b7d5d5d9acb7572dfcd062ef237ec3afa77c3a3
Diana Eastman <deastman@mitre.org>
2022-05-12 13:00:03 -0400

    Add AHRQ headers and assets.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,13 @@
 module ApplicationHelper
   # Adapted from: https://stackoverflow.com/questions/9879169/how-to-get-twitter-bootstrap-navigation-to-show-active-link
   def nav_bar(&block)
-    tag.ul(class: 'nav navbar-nav', &block)
+    tag.ul(class: 'menu nav navbar-nav cds-connect-nav', &block)
   end
 
   def nav_link(text, path)
-    options = current_page?(path) ? { class: 'nav-item active' } : { class: 'nav-item' }
+    options = current_page?(path) ? { class: 'is-active' } : { class: '' }
     tag.li(**options) do
-      link_to text, path, class: 'nav-link'
+      link_to text, path
     end
   end
 
