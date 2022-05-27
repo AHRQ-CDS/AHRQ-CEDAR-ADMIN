43750f1524a2128e4f7a78e8ba805f6228036283
Diana Eastman <deastman@mitre.org>
2022-05-27 14:06:27 -0400

    Update error message.

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -5,6 +5,6 @@ module Utilities
   def parse_date_string(date_string, error_context)
     Date.parse(date_string) unless date_string.nil?
   rescue Date::Error
-    (warnings << error_context) || 'Unspecified error'
+    (warnings << "#{error_context}, #{date_string}") || 'Unspecified error date error'
   end
 end
