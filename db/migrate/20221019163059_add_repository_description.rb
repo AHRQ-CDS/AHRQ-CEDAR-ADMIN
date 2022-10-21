6fd7edac0843cb8a7f4b1a9416fb54f9954b07aa
Yunwei Wang <>
2022-10-21 14:01:43 -0500

    Add migration for Repository.description

--- db/migrate/20221019163059_add_repository_description.rb
+++ db/migrate/20221019163059_add_repository_description.rb
@@ -1,13 +1,25 @@
 class AddRepositoryDescription < ActiveRecord::Migration[6.0]
   def change
     add_column :repositories, :description, :string
-    Repository.find_each do |repository|
-      case repository.fhir_id
-      when 'uspstf'
-        repository.description = 'Volunteer panel of experts developing evidence-based recommendations about clinical recommendations'
-      # other repositories here #
-      end
-      repository.save!
-    end
+
+    Repository.find_by(fhir_id: 'uspstf')&.update(description:
+      'The United States Preventive Services Task Force is an independent, ' \
+      'volunteer panel of experts in prevention and evidence-based medicine.')
+
+    Repository.find_by(fhir_id: 'cds-connect')&.update(description:
+      'CDS Connect provides Clinical Decision Support artifacts that are based on clinical practice guidelines, ' \
+      'peer-reviewed articles, best practices, and other content identified via PCOR.')
+
+    Repository.find_by(fhir_id: 'ehc')&.update(description:
+      'The AHRQ EHC Program\'s goal is to improve healthcare quality by enabling access to the best available evidence ' \
+      'on outcomes and appropriateness of healthcare treatments, devices, and services.')
+
+    Repository.find_by(fhir_id: 'epc')&.update(description:
+      'Evidence-based Practice Centers are academic and other research institutions contracted by the EHC Program ' \
+      'to evaluate and summarize healthcare evidence.')
+
+    Repository.find_by(fhir_id: 'srdr')&.update(description:
+      'The SRDR is a collaborative, web-based resource containing systematic review data that ' \
+      'functions as both a data repository and a data extraction tool.')
   end
 end
