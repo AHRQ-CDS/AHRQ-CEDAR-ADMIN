10aba215507126c928d5fb2dcde9a3588b1e8fd9
Marc Hadley <mhadley@mitre.org>
2022-11-28 16:08:02 +0000

    Fix for CEDAR-790. Update farady gem to more recent version, set a bot-style User-Agent HTTP header since the NIH web server that hosts archived EPC artifacts seems to block requests that use the default faraday user agent.

---
+++ config/initializers/faraday_connection.rb
@@ -0,0 +1,2 @@
+# Set up default connection parameters for Faraday
+Faraday.default_connection_options.headers = { "User-Agent" => 'Mozilla/5.0 (compatible; Cedarbot/1.0; +https://cds.ahrq.gov/cedar)' }
