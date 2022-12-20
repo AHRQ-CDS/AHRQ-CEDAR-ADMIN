ceb582023eb83e05795dc858e0b2b9697c0a10b6
Marc Hadley <mhadley@mitre.org>
2022-12-20 00:30:52 +0000

    Rubocop fix

--- test/system/import_runs_test.rb
+++ test/system/import_runs_test.rb
@@ -10,8 +10,8 @@ class ImportRunsTest < ApplicationSystemTestCase
     @repository = create_repository_with_artifacts(count: 2)
     @start = Time.current
     @import_run = create(:import_run, repository: @repository, start_time: @start,
-                         end_time: Time.current, total_count: 2, new_count: 0, update_count: 0,
-                         delete_count: 0, status: :success)
+                                      end_time: Time.current, total_count: 2, new_count: 0, update_count: 0,
+                                      delete_count: 0, status: :success)
   end
 
   test 'import_run functions as expected' do
