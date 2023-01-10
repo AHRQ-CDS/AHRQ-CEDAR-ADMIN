2acc9bc2cb98495358bda35f1a229ecdeaac1cae
Pete K <krautscheid@mitre.org>
2023-01-10 17:42:41 -0500

    Make email settings more configurable

--- config/application.rb
+++ config/application.rb
@@ -47,9 +47,9 @@ module CedarAdmin
     config.ngc_base_url = ENV['CEDAR_NGC_BASE_URL']
 
     # Mailer settings
-    config.action_mailer.default_url_options = { host: 'cds.ahrq.gov' }
-    config.cedar_from_email = 'cedar@cds.ahrq.gov'
-    config.cedar_to_email = 'cedar@cds.ahrq.gov'
+    config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] || 'cds.ahrq.gov' }
+    config.cedar_from_email = ENV['CEDAR_FROM_EMAIL'] || 'cedar@ahrq.hhs.gov'
+    config.cedar_to_email = ENV['CEDAR_TO_EMAIL'] || 'cedar@ahrq.hhs.gov'
 
     config.time_zone = 'Eastern Time (US & Canada)'
 
