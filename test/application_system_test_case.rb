22d4521d8a99960185c15808f25b4770f35fa942
Marc Hadley <mhadley@mitre.org>
2023-02-09 20:59:22 +0000

    Fix system tests.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,9 +4,7 @@ require 'test_helper'
 require 'axe/matchers/be_axe_clean'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
-    args: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage']
-  }
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
   include Devise::Test::IntegrationHelpers
 
