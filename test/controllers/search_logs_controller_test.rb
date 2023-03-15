833496a441306066c588cabb0909efe2fc32d24f
Pete K <krautscheid@mitre.org>
2023-03-15 12:30:42 -0400

    Refactor tests to correct intermittent failure due to time jitter

--- test/controllers/search_logs_controller_test.rb
+++ test/controllers/search_logs_controller_test.rb
@@ -5,16 +5,22 @@ require 'test_helper'
 class SearchLogsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test 'should get index' do
-    sign_in create(:user)
+  def create_search_log
+    start_time = Time.zone.now
+    end_time = start_time + 1.second
     create(:search_log,
            search_params: { '_content' => 'statin', 'artifact-current-state' => 'active' },
            count: 10,
            total: 27,
            client_ip: '192.168.1.1',
-           start_time: Time.zone.now,
-           end_time: 1.second.from_now,
+           start_time: start_time,
+           end_time: end_time,
            returned_artifact_ids: [2565, 2989, 2943, 3086, 3087, 2945, 2944, 2306, 508, 3036])
+  end
+
+  test 'should get index' do
+    sign_in create(:user)
+    create_search_log
     get search_logs_url
     assert_response :success
     assert_equal 1, assigns(:search_logs).count
@@ -22,14 +28,7 @@ class SearchLogsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should get index with ip' do
     sign_in create(:user)
-    create(:search_log,
-           search_params: { '_content' => 'statin', 'artifact-current-state' => 'active' },
-           count: 10,
-           total: 27,
-           client_ip: '192.168.1.1',
-           start_time: Time.zone.now,
-           end_time: 1.second.from_now,
-           returned_artifact_ids: [2565, 2989, 2943, 3086, 3087, 2945, 2944, 2306, 508, 3036])
+    create_search_log
     get search_logs_url(ip: '192.168.1.1')
     assert_equal 1, assigns(:search_logs).count
     get search_logs_url(ip: '192.168.1.2')
--- test/controllers/search_stats_controller_test.rb
+++ test/controllers/search_stats_controller_test.rb
@@ -5,16 +5,22 @@ require 'test_helper'
 class SearchStatsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test 'should get index' do
-    sign_in create(:user)
+  def create_search_log
+    start_time = Time.zone.now
+    end_time = start_time + 1.second
     create(:search_log,
            search_params: { '_content' => 'statin', 'artifact-current-state' => 'active' },
            count: 10,
            total: 27,
            client_ip: '192.168.1.1',
-           start_time: Time.zone.now,
-           end_time: 1.second.from_now,
+           start_time: start_time,
+           end_time: end_time,
            returned_artifact_ids: [2565, 2989, 2943, 3086, 3087, 2945, 2944, 2306, 508, 3036])
+  end
+
+  test 'should get index' do
+    sign_in create(:user)
+    create_search_log
     get search_stats_url
     assert_response :success
     assert_equal 1, assigns(:search_count)
@@ -25,14 +31,7 @@ class SearchStatsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should get index with date range' do
     sign_in create(:user)
-    create(:search_log,
-           search_params: { '_content' => 'statin', 'artifact-current-state' => 'active' },
-           count: 10,
-           total: 27,
-           client_ip: '192.168.1.1',
-           start_time: Time.zone.now,
-           end_time: 1.second.from_now,
-           returned_artifact_ids: [2565, 2989, 2943, 3086, 3087, 2945, 2944, 2306, 508, 3036])
+    create_search_log
     get search_stats_url(start_date: Time.zone.today - 10.days, end_date: Time.zone.today - 5.days)
     assert_response :success
     assert_equal 0, assigns(:search_count)
@@ -43,14 +42,7 @@ class SearchStatsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should get index with ip exclusion' do
     sign_in create(:user)
-    create(:search_log,
-           search_params: { '_content' => 'statin', 'artifact-current-state' => 'active' },
-           count: 10,
-           total: 27,
-           client_ip: '192.168.1.1',
-           start_time: Time.zone.now,
-           end_time: 1.second.from_now,
-           returned_artifact_ids: [2565, 2989, 2943, 3086, 3087, 2945, 2944, 2306, 508, 3036])
+    create_search_log
     get search_stats_url(exclude_ips: '192.168.1.1')
     assert_response :success
     assert_equal 0, assigns(:search_count)
